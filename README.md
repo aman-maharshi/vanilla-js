9a91eabfbf14f5d52945b0c4f587a77b31c06c4c
aman-maharshi <amanm4harshi@gmail.com>
2019-03-24 15:37:31 +0530

    drag - over,enter,leave,drop functions added

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. [Weight Converter](https://aman-maharshi.github.io/vanilla-js/weight-converter/)
 2. [Filterable List](https://aman-maharshi.github.io/vanilla-js/filterable-list/)
+3. [Drag n Drop](https://aman-maharshi.github.io/vanilla-js/drag-n-drop/)
--- drag-n-drop/script.js
+++ drag-n-drop/script.js
@@ -1,13 +1,43 @@
-let fill = document.querySelector('.fill');
+// VARIABLES
+let fill = document.querySelector('.fill'),
+    emptyBox = document.querySelectorAll('.empty');
 
+// EVENT LISTENERS
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
 
+// to call 4 different events for each box
+emptyBox.forEach(function(box) {
+    box.addEventListener('dragover', dragOver);
+    box.addEventListener('dragenter', dragEnter);
+    box.addEventListener('dragleave', dragLeave);
+    box.addEventListener('drop', dragDrop);
+});
+
+
+// FUNCTIONS
 function dragStart() {
     this.className += ' hold';
+    // hiding the image from the box when dragStart triggers
     setTimeout(() => (this.className = 'invisible'),0);
 }
-
 function dragEnd() {
+    // putting the image back when dragEnd triggers
     this.className = 'fill'
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered'
+}
+function dragLeave() {
+    this.className = 'empty';
+}
+function dragDrop() {
+    this.className = 'empty';
+    // this.className += ' fill';
+    this.append(fill); // appending the entire div    
 }
