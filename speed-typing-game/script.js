b5f70a60dd7e3532b5eaae37a008e64c5f9e3340
aman-maharshi <amanm4harshi@gmail.com>
2019-03-31 20:39:31 +0530

    init function added

--- speed-typing-game/script.js
+++ speed-typing-game/script.js
@@ -3,3 +3,19 @@
     - add a difficulty level options (time left = 5s/3s/2s)
     - load random words from an API
  */
+
+ let words = ['magic', 'journey', 'travel', 'explore', 'life', 
+'experience', 'happiness', 'gratitude', 'discipline', 'exercise', 
+'workout', 'friendship', 'practice', 'routine', 'morning', 'reading', 
+'books', 'education', 'amour', 'delibrate', 'protein', 'partner',
+'empathy', 'concert', 'patience', 'humor', 'resilience', 'confidence',
+'consistency'];
+ let currentWord  = document.querySelector('#current-word');
+
+ window.addEventListener('load', init);
+
+
+ function init() {
+     let index = Math.floor(Math.random() * words.length);
+     currentWord.textContent = words[index];
+ }
