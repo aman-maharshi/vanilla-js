1a15ff7f311c1aa172ceb40ee62209e58e1c4b26
aman-maharshi <amanm4harshi@gmail.com>
2019-04-04 23:19:48 +0530

    search todo items fucntion added

---
+++ todos/README.md
@@ -0,0 +1,12 @@
+# toDos
+### simple todo list app with a lot of functionalities
+
+Current Features:
+* Add todo items
+* Remove selected item
+* Search through the todo list
+
+Features to add:
+*  Store the todo items to loacal storage
+* Load items from local storage
+
--- todos/script.js
+++ todos/script.js
@@ -1,17 +1,25 @@
+/*
+    TODO:
+    - save items to local storage
+    - load list items from local storage on page load/ refresh
+*/
+
 // VARIABLES
 let todoItems = document.getElementById('todo-items'),
     todoInput = document.getElementById('todo-input'),
-    todoForm = document.getElementById('todo-form');
+    todoForm = document.getElementById('todo-form'),
+    todoSearch = document.getElementById('todo-search');
 
 
 // EVENT LISTENERS
 todoItems.addEventListener('click', removeItem);
 todoForm.addEventListener('submit', addTodo);
+todoSearch.addEventListener('keyup', searchItem); 
+
 
 // FUNCTIONS
-
-function removeItem(e) {
-    if(e.target.classList.contains('btn')) {
+function removeItem(e) { 
+    if(e.target.classList.contains('btn'))  {
         e.target.parentElement.remove();
     }
 }
@@ -35,5 +43,19 @@ function addTodo(e) {
         // appending the todo item to the list
         todoItems.appendChild(li);
         todoForm.reset();
+    }
+}
+
+function searchItem() {
+    let searchInput = todoSearch.value.toUpperCase();
+    let items = todoItems.getElementsByTagName('li');
+    for(let i = 0; i < items.length; i++) {
+        let val = items[i].textContent.toUpperCase();
+        if(val.indexOf(searchInput) != -1) {
+            items[i].style.display = 'block';
+        }
+        else{
+            items[i].style.display = 'none';
+        }
     }
 }
