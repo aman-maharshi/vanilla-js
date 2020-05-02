3445aaa0736fd69ef74ea1d67138c6c79201006e
aman-maharshi <amanm4harshi@gmail.com>
2020-05-03 02:33:37 +0530

    type and erase functionality complete

--- typing-effect/script.js
+++ typing-effect/script.js
@@ -1,21 +1,51 @@
-const typedTextSpan = document.querySelector('.typed-text')
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
 
 const textArray = ['Web Developer', 'Programmer', 'Musician'];
 const typingDelay = 200;
-const easingDelay = 100;
-const newTextDelay = 2000;
+const erasingDelay = 100;
+const newTextDelay = 3000;
 let textArrayIndex = 0;
 let charIndex = 0;
 
 function type() {
+    if(!(cursorSpan.classList.contains('typing'))) {
+        cursorSpan.classList.add('typing');
+    }
     // if last letter of a word is not typed
     if(charIndex < textArray[textArrayIndex].length) {
-        typedTextSpan.textContent +=textArray[textArrayIndex].charAt(charIndex);
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
         charIndex++;
         setTimeout(type, typingDelay);
     }
     else {
-        // erase
+        cursorSpan.classList.remove('typing');
+        setTimeout(erase, newTextDelay);
     }
 }
-type();
+
+function erase() {
+    if(!(cursorSpan.classList.contains('typing'))) {
+        cursorSpan.classList.add('typing');
+    }
+    // if string is not entirely erased
+    if(charIndex > 0) { 
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        cursorSpan.classList.remove('typing');
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) {
+            textArrayIndex = 0;
+        }
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    if(textArray.length) {
+        setTimeout(type, newTextDelay + 250);
+    }
+})
