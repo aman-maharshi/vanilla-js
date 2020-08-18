458bf5fa448d446fd41cf7290076a92b9198c03e
aman-maharshi <amanm4harshi@gmail.com>
2020-08-18 20:03:56 +0530

    functionality complete

---
+++ weather-app/app.js
@@ -0,0 +1,29 @@
+const apiKey = "16f00dd6edca31d9934c7fd9cdfba77c"
+const baseUrl = "http://api.openweathermap.org/data/2.5/weather?"
+
+const form = document.querySelector(".inputForm")
+
+form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e) {
+    e.preventDefault()
+    const city = document.querySelector(".inputCity").value
+    fetchWeatherData(city)
+}
+
+async function fetchWeatherData(city) {
+    const apiResponse = await fetch(`${baseUrl}q=${city}&units=metric&APPID=${apiKey}`)
+    const weatherData = apiResponse.json()
+    weatherData.then(data => {
+        //console.log(data)
+        document.querySelector(".city").textContent = data.name
+        document.querySelector(".country").textContent = `, ${data.sys.country}`
+        document.querySelector(".temprature").textContent = `${data.main.temp} °c`
+        document.querySelector(".sky").textContent = data.weather[0].main
+        document.querySelector(".min-max").textContent = `${data.main.temp_min}°c / ${data.main.temp_max}°c`
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchWeatherData("Delhi")
+})
