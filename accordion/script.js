1aca472f8d842801a78f51b47c3cf6b331d34bce
aman-maharshi <amanm4harshi@gmail.com>
2020-08-22 22:42:56 +0530

    version one functionality and styling complete

---
+++ accordion/script.js
@@ -0,0 +1,17 @@
+const accordionHeaders = document.querySelectorAll(".accordion-item-header")
+const accordionBodies = document.querySelectorAll(".accordion-item-body")
+
+accordionHeaders.forEach(item => {
+    item.addEventListener("click", e => {
+        itemHeader = e.target
+        itemBody = e.target.nextElementSibling
+        itemHeader.classList.toggle("active")
+        itemBody.classList.toggle("hide")
+    })
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    accordionBodies.forEach(item => {
+        item.classList.add("hide")
+    })
+})
