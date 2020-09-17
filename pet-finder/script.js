447d67d828bc468f2ea5cce14f6ace1785eb85fd
aman-maharshi <amanm4harshi@gmail.com>
2020-09-17 19:03:28 +0530

    dog name and image fetch complete

---
+++ pet-finder/script.js
@@ -0,0 +1,32 @@
+let dropDown = document.querySelector(".breeds")
+let imageContainer = document.querySelector(".slideshow")
+
+async function getPetName() {
+    let response = await fetch("https://dog.ceo/api/breeds/list/all")
+    let data = await response.json()
+    return data
+}
+
+async function getPetImage(pet) {
+    let response = await fetch(`https://dog.ceo/api/breed/${pet}/images/random`)
+    let data = await response.json()
+    return data
+}
+
+function addPetNamesToHtml() {
+    getPetName().then(data => {
+        let breeds = Object.keys(data.message)
+        breeds.forEach(item => {
+            dropDown.innerHTML += `<option value=${item}>${item}</option>`
+        })
+    })
+}
+
+addPetNamesToHtml()
+
+dropDown.addEventListener("change", e => {
+    getPetImage(e.target.value).then(data => {
+        let url = data.message
+        imageContainer.innerHTML = `<img src="${url}">`
+    })
+})
