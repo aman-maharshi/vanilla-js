9ce534116e5517bc0681ba6300a467f7a24479cb
aman-maharshi <amanm4harshi@gmail.com>
2020-09-19 16:19:22 +0530

    minor style change

--- filterable-list-v2/script.js
+++ filterable-list-v2/script.js
@@ -1,17 +1,18 @@
-let myInput = document.querySelector('.custom-input');
-let myList = document.querySelectorAll('.custom-list li a');
+let myInput = document.querySelector(".custom-input")
+let myList = document.querySelectorAll(".custom-list li a")
 
-myInput.addEventListener('input', () => {
-    myList.forEach((val) => {
-        console.log(myInput.value.toLowerCase());
-        if(val.textContent.toLowerCase().indexOf(myInput.value.toLowerCase()) == -1) {
-            val.parentElement.style.display = 'none';
+window.addEventListener("load", () => {
+    myInput.focus()
+})
+
+myInput.addEventListener("input", () => {
+    myList.forEach(val => {
+        console.log(myInput.value.toLowerCase())
+        if (val.textContent.toLowerCase().indexOf(myInput.value.toLowerCase()) == -1) {
+            val.parentElement.style.display = "none"
         }
-        if(myInput.value == "") {
-            val.parentElement.style.display = "block";
+        if (myInput.value == "") {
+            val.parentElement.style.display = "block"
         }
-    });
-});
-
-
-
+    })
+})
